8e1b26ff8e57bc25323627ca0696ba0a3df4f806
albmarqu <albmarqu@student.42malaga.com>
2024-08-04 14:19:24 +0200

    Nuevo repositorio para tener solo el cursus

---
+++ 5. minitalk/minitalk.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: albmarqu <albmarqu@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/17 19:51:09 by albmarqu          #+#    #+#             */
+/*   Updated: 2024/07/17 20:31:09 by albmarqu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <unistd.h>
+# include <signal.h>
+
+// Añadir lasfunciones creadas
+
+#endif
