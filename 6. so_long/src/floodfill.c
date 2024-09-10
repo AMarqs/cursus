0bfbabea4cfcf25ec674cb0d8f5a0d9a1a101116
AlbaMarq <albamarquesr@gmail.com>
2024-09-10 19:21:51 +0200

    Archivos pulidos para norminette

--- 6. so_long/src/floodfill.c
+++ 6. so_long/src/floodfill.c
@@ -6,11 +6,18 @@
 /*   By: albmarqu <albmarqu@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 21:45:13 by albmarqu          #+#    #+#             */
-/*   Updated: 2024/09/10 14:01:28 by albmarqu         ###   ########.fr       */
+/*   Updated: 2024/09/10 19:17:41 by albmarqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+
+void	free_copy(char **map_copy, int row)
+{
+	while (row >= 0)
+		free(map_copy[row--]);
+	free(map_copy);
+}
 
 void	floodfill(char **map, int x, int y)
 {
@@ -23,24 +30,32 @@ void	floodfill(char **map, int x, int y)
 	floodfill(map, x, y - 1);
 }
 
-void	free_copy(char **map_copy, int row)
+char	**copy_map(t_map *map)
 {
-	while (row >= 0)
-		free(map_copy[row--]);
-	free(map_copy);
-}
-
-bool	flood(t_map *map, int x, int y)
-{
-	char 	**map_copy;
+	char	**map_copy;
 	int		i;
-	int		j;
 
 	map_copy = malloc((map->row + 1) * sizeof(char *));
+	if (map_copy == NULL)
+	{
+		write(2, "Error\nError allocating memory\n", 30);
+		free_map(map);
+		exit(EXIT_FAILURE);
+	}
 	i = -1;
 	while (++i < map->row)
 		map_copy[i] = ft_strdup(map->map[i]);
 	map_copy[map->row] = NULL;
+	return (map_copy);
+}
+
+bool	flood(t_map *map, int x, int y)
+{
+	char	**map_copy;
+	int		i;
+	int		j;
+
+	map_copy = copy_map(map);
 	floodfill(map_copy, x, y);
 	i = -1;
 	while (++i < map->row)
@@ -63,7 +78,6 @@ bool	path(t_map *map)
 {
 	int	i;
 	int	j;
-	
 
 	map->x = 0;
 	map->y = 0;
--- 6. so_long/src/so_long.h
+++ 6. so_long/src/so_long.h
@@ -6,7 +6,7 @@
 /*   By: albmarqu <albmarqu@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/08 22:51:12 by albmarqu          #+#    #+#             */
-/*   Updated: 2024/09/10 14:29:33 by albmarqu         ###   ########.fr       */
+/*   Updated: 2024/09/10 19:15:16 by albmarqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,13 @@
 # include <sys/types.h>
 # include <sys/uio.h>
 # include <stdlib.h>
-# include <stdio.h> ///
+# include <stdio.h>
 # include <math.h>
 # include <stdint.h>
 # include <limits.h>
-# include "MLX42/include/MLX42/MLX42.h"
-# include "libft/libft.h"
-# include "ft_printf/ft_printf.h"
+# include "../MLX42/include/MLX42/MLX42.h"
+# include "../libft/libft.h"
+# include "../ft_printf/ft_printf.h"
 # include "get_next_line_bonus.h"
 
 # ifndef HEIGHT
@@ -53,8 +53,8 @@ typedef struct s_image
 	mlx_image_t		*i_floor;
 	mlx_texture_t	*t_wall;
 	mlx_image_t		*i_wall;
-	mlx_image_t		*i_char;
-	mlx_texture_t	*t_char;
+	mlx_texture_t	*t_play;
+	mlx_image_t		*i_play;
 	mlx_texture_t	*t_col;
 	mlx_image_t		*i_col;
 	mlx_texture_t	*t_exit;
@@ -65,13 +65,41 @@ typedef struct s_game
 {
 	mlx_t		*mlx;
 	t_map		*map;
+	t_image		*image;
 	int			cont;
 }	t_game;
 
 char	*get_next_line(int fd);
+
+int		ft_strlen2(const char *s);
+void	free_map(t_map *map);
+void	free_image(t_image *image);
+
+void	check_args(int argc, char **argv);
+void	check_file(char **argv, t_map *map);
+void	fill_map(int file, t_map *map);
+void	create_map(char **argv, t_map *map);
+
 bool	border(char **map, int row, int col);
+bool	characters(t_map *map);
 bool	check_characters(t_map *map);
+void	check_map(t_map *map);
+
+void	free_copy(char **map_copy, int row);
+void	floodfill(char **map, int x, int y);
+char	**copy_map(t_map *map);
+bool	flood(t_map *map, int x, int y);
 bool	path(t_map *map);
-void	mapping(t_map *map, mlx_t *mlx);
+
+void	load_texture(t_image *image, t_game *game);
+void	load_image(t_image *image, mlx_t *mlx, t_game *game);
+void	display(t_game *game, mlx_image_t *image, int x, int y);
+void	init_image(t_game *game);
+void	mapping(t_game *game);
+
+void	f_close(void *param);
+void	move(t_game *game, int x, int y);
+void	f_key(mlx_key_data_t keydata, void *param);
+t_game	*init_game(t_map *map);
 
 #endif
